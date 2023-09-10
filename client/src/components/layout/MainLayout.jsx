21bf70e06646084bf2fa73b1597c74bd44c41185
Rajeev <rajeevkr8917@gmail.com>
2023-09-11 01:20:55 +0530

    global loading added

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import GlobalLoading from '../common/GlobalLoading';
 
 const MainLayout = () => {
     return (
         <>
+            <GlobalLoading />
             <Box display="flex" minHeight="100vh">
                 <Box
                     component="main"
