f4469e67568e5b7cb527e4481d2ef0ac6fd5c91a
Rajeev <rajeevkr8917@gmail.com>
2023-09-11 01:44:44 +0530

    footer added

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import GlobalLoading from '../common/GlobalLoading';
+import Footer from '../common/Footer';
+import Topbar from '../common/Topbar';
 
 const MainLayout = () => {
     return (
         <>
             <GlobalLoading />
             <Box display="flex" minHeight="100vh">
+                <Topbar />
                 <Box
                     component="main"
                     flexGrow={1}
@@ -17,6 +20,8 @@ const MainLayout = () => {
                     <Outlet />
                 </Box>
             </Box>
+
+            <Footer />
         </>
     )
 };
